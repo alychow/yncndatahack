1748cc094709aaf0f9818672122ce9c2ac7f6391
Connor Lawless <connor.lawless@hotmail.com>
2017-01-08 13:33:49 -0500

    Preliminary Data Cleaning

--- API_Authentication.R
+++ API_Authentication.R
@@ -1,3 +1,13 @@
 library(readr)
+library(stringr)
+library(dplyr)
 
 transactions <- read_csv("invoice_items.csv")
+transactions[transactions$product=="PepsiCo Expo" | transactions$product == "Microsoft x YouX Expo" | transactions$product == "P&G CBD Expo Marketin",6] <- "LP Company Event"
+
+transactions <- mutate(transactions, startup = str_detect(transactions$product, "[eE]xpo|Startup Gold"))
+transactions <- mutate(transactions, corp = str_detect(transactions$product, "Career Fair"))
+transactions <- mutate(transactions, LP = str_detect(transactions$product, "LP"))
+
+tmp <- filter(transactions, !(startup | corp | LP))
+tmp2 <- filter(transactions, startup)
