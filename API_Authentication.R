50119698cef79757517bfa25864fc84f362b9a73
alychow <alisonchow@Alisons-MacBook-Air.local>
2017-01-08 13:42:15 -0500

    Finished transactions for corporate+startup

--- API_Authentication.R
+++ API_Authentication.R
@@ -6,8 +6,9 @@ transactions <- read_csv("invoice_items.csv")
 transactions[transactions$product=="PepsiCo Expo" | transactions$product == "Microsoft x YouX Expo" | transactions$product == "P&G CBD Expo Marketin",6] <- "LP Company Event"
 
 transactions <- mutate(transactions, startup = str_detect(transactions$product, "[eE]xpo|Startup Gold"))
-transactions <- mutate(transactions, corp = str_detect(transactions$product, "Career Fair"))
-transactions <- mutate(transactions, LP = str_detect(transactions$product, "LP"))
+transactions <- mutate(transactions, corporate = str_detect(transactions$product, "Career Fair|CF"))
 
-tmp <- filter(transactions, !(startup | corp | LP))
-tmp2 <- filter(transactions, startup)
+tmp <- filter(transactions, !(startup | corporate))
+tmpStartup <- filter(transactions, startup)
+tmpCorporate <- filter(transactions,corporate)
+
